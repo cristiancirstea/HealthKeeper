909ae56f94d7f2ddceeb476769d8fe406d502fd3
Alexandru Marian Grigoroiu <grigoroiualexandru@gmail.com>
2014-03-28 22:01:46 +0200

    Va ignore codurile qr generate. Cu un pas mai aproape de generarea codului QR corect

--- PHP/WS/library/js/qrcode.js
+++ PHP/WS/library/js/qrcode.js
@@ -1,3 +1,40 @@
 jQuery(document).ready(function() {
-    
+    $('.form-control').on('blur', function() {
+        validateInput();
+    });
+
+    $('#generate-qr').on('click', function() {
+        $.ajax({
+            type: 'POST',
+            url: '../ajax/generateqrcode.php',
+            data: { medicineName: $('#medicineName').val(),
+                    hStart :      $('#hStart').val(),
+                    period :      $('#period').val(),
+                    span :        $('#span').val(),
+                    importance:   $('#importance').val()
+                  }
+        })
+        .done(function(data) {
+            if(data){
+                data = JSON.parse(data);
+                console.log(data.qrPath);
+            } else {
+                console.log('Nasol');
+            }
+        });
+    });
 });
+
+function validateInput() {
+    var medicineName = $('#medicineName').val(),
+        hStart = $('#hStart').val(),
+        period = $('#period').val(),
+        span = $('#span').val(),
+        importance = $('#importance').val();
+
+    if(medicineName && hStart && period && span && importance) {
+        $('#generate-qr').removeClass('btn-danger').addClass('btn-success');
+    } else {
+        $('#generate-qr').removeClass('btn-success').addClass('btn-danger');
+    }
+}
